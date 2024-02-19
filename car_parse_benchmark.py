27e606b4cd66944e3032593be1330f259455976a
David Buchanan <d@vidbuchanan.co.uk>
2024-02-19 08:22:53 +0000

    benchmark encode and decodes

--- car_parse_benchmark.py
+++ car_parse_benchmark.py
@@ -1,5 +1,6 @@
 import io
 import sys
+import time
 from cbrrr import parse_dag_cbor, encode_dag_cbor
 
 sys.setrecursionlimit(99999999)
@@ -15,7 +16,13 @@ def parse_varint(stream):
 			return n
 		shift += 7
 
+enctime = 00
+dectime = 0
+
 def parse_car(stream, length):
+	global enctime
+	global dectime
+
 	header_len = parse_varint(stream)
 	header_bytes = stream.read(header_len)
 	assert(len(header_bytes) == header_len)
@@ -35,8 +42,13 @@ def parse_car(stream, length):
 		assert(len(block_data) == block_len-36)
 		#content_hash = hashlib.sha256(block_data).digest()
 		#assert(cid_raw.endswith(content_hash))
+		start = time.time()
 		block = parse_dag_cbor(block_data)
+		#block = libipld.decode_dag_cbor(block_data)
+		dectime += time.time()-start
+		start = time.time()
 		roundtrip = encode_dag_cbor(block)
+		enctime += time.time()-start
 		assert(block_data == roundtrip)
 		#print(block)
 		nodes[cid_raw] = block
@@ -45,13 +57,20 @@ def parse_car(stream, length):
 
 if __name__ == "__main__":
 	import sys
-	import time
 
 	car = open(sys.argv[1], "rb").read()
 	start_time = time.time()
 
 	root, nodes = parse_car(io.BytesIO(car), len(car))
 
-	duration = time.time()-start_time
-	speed = (len(car)/(1024*1024))/duration
-	print(f"Parsed {len(car)} bytes at {speed:.2f}MB/s")
+	dec_speed = (len(car)/(1024*1024))/dectime
+	print(f"Parsed {len(car)} bytes at {dec_speed:.2f}MB/s")
+
+	enc_speed = (len(car)/(1024*1024))/enctime
+	print(f"Encoded {len(car)} bytes at {enc_speed:.2f}MB/s")
+
+	#start = time.time()
+	#libipld.decode_car(car)
+	#duration = time.time()-start
+	#car_speed = (len(car)/(1024*1024))/duration
+	#print(f"libipld.decode_car {len(car)} bytes at {car_speed:.2f}MB/s")
