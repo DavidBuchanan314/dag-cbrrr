d6b1437f325b79540877349a8de79412f0a09d56
David Buchanan <d@vidbuchanan.co.uk>
2024-02-23 22:59:01 +0000

    add atheris fuzz harness

---
+++ atheris_fuzz.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3.10
+
+import atheris
+import sys
+
+with atheris.instrument_imports():
+	import cbrrr
+
+ATJSON_MODE = True
+
+def TestOneInput(data):
+	try:
+		roundtrip = cbrrr.encode_dag_cbor(cbrrr.decode_dag_cbor(data, ATJSON_MODE), ATJSON_MODE)
+	except:
+		return
+	if not ATJSON_MODE: # atjson does not roundtrip consistently!!!
+		assert(roundtrip==data)
+
+atheris.Setup(sys.argv, TestOneInput)
+atheris.Fuzz()
