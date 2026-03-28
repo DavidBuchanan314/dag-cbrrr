26389199d59b85b377579f70af507dcd1d901b0a
David Buchanan <d@vidbuchanan.co.uk>
2026-03-28 19:10:07 +0000

    support 32-bit platforms

--- src/cbrrr/_cbrrr.c
+++ src/cbrrr/_cbrrr.c
@@ -5,21 +5,8 @@
 #include <stdint.h>
 #include <limits.h>
 
-#define STATIC_ASSERT(COND,MSG) typedef char static_assertion_##MSG[(COND)?1:-1]
-
 // returns true if a * b would overflow size_t
 #define SIZE_MUL_OVERFLOW(a, b) ((b) != 0 && (a) > SIZE_MAX / (b))
-
-/* If you're compiling on a 32-bit platform, commenting this out should "work",
-   but I make no guarantees about the safety of the resulting code. I think
-   there are some lurking integer-overflow-adjacent bugs that could be
-   triggered by malicious inputs. (frankly, the same caveat applies to 64-bit
-   too, but I've at least fuzz-tested it a bit)
-
-   It should be safe enough if you're self-hosting a PDS, but think twice about
-   e.g. parsing the atproto firehose.
-*/
-STATIC_ASSERT(sizeof(size_t) == 8, _64bit_platforms_only); // this'll hopefully be relaxed in the future
 
 // XXX: not sure having these as globals is the right thing to do?
 static PyObject *PY_ZERO;
